4e59ddc8fc353bc1effa08091936f5a055dc6b7d
amerharb <amer.harb@gmail.com>
2016-03-11 16:51:15 +0100

    add new classes

---
+++ app/src/main/java/malmoteam/dailyactive/daONTask.java
@@ -0,0 +1,8 @@
+package malmoteam.dailyactive;
+
+/**
+ * Created by Amer on 03-Mar-16.
+ */
+public class daONTask extends daTask {
+
+}
---
+++ app/src/main/java/malmoteam/dailyactive/daTask.java
@@ -0,0 +1,16 @@
+package malmoteam.dailyactive;
+
+/**
+ * Created by Amer on 03-Mar-16.
+ */
+public abstract class daTask {
+    private String taskName;
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+}
