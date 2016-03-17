680086c07841cf4e086c3a50842294c633b5ce8e
wasim <wasimijaz@hotmail.com>
2016-03-17 19:41:02 +0100

    add relative layout in main screen ...four button OI,DI,ON,DN and add four activities with layout so by clicking each button it,s goin to next activity...feed back amer??

--- app/src/main/java/malmoteam/dailyactive/MainActivity.java
+++ app/src/main/java/malmoteam/dailyactive/MainActivity.java
@@ -1,7 +1,10 @@
 package malmoteam.dailyactive;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,72 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button nextScreen = (Button) findViewById(R.id.oi);
+        Button nextScreen2 = (Button) findViewById(R.id.di);
+        Button nextScreen3 = (Button) findViewById(R.id.on);
+        Button nextScreen4 = (Button) findViewById(R.id.dn);
+
+        //Listening to button event
+        nextScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Starting a new Intent
+                //saveInfo();
+                Intent intent = new Intent(MainActivity.this, OI.class);
+                //Intent intent = new Intent(FirstScreenActivity.this, SecondScreenActivity.class);
+                startActivity(intent);
+                //startActivity(intent);
+
+            }
+        });
+
+        nextScreen2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Starting a new Intent
+                //saveInfo();
+                Intent intent = new Intent(MainActivity.this, DI.class);
+                //Intent intent = new Intent(FirstScreenActivity.this, SecondScreenActivity.class);
+                startActivity(intent);
+                //startActivity(intent);
+
+            }
+        });
+
+        nextScreen3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Starting a new Intent
+                //saveInfo();
+                Intent intent = new Intent(MainActivity.this, ON.class);
+                //Intent intent = new Intent(FirstScreenActivity.this, SecondScreenActivity.class);
+                startActivity(intent);
+                //startActivity(intent);
+
+            }
+        });
+
+        nextScreen4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Starting a new Intent
+                //saveInfo();
+                Intent intent = new Intent(MainActivity.this, DN.class);
+                //Intent intent = new Intent(FirstScreenActivity.this, SecondScreenActivity.class);
+                startActivity(intent);
+                //startActivity(intent);
+
+            }
+        });
+
+
+
+
+
+
+
+
     }
 }
