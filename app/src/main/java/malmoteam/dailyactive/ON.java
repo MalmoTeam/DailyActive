9a791e531611b5fde5c51a7f71914baad9338fbc
wasim <wasimijaz@hotmail.com>
2016-03-18 11:36:22 +0100

    all screen has back button to going back to main activity

--- app/src/main/java/malmoteam/dailyactive/ON.java
+++ app/src/main/java/malmoteam/dailyactive/ON.java
@@ -1,11 +1,9 @@
 package malmoteam.dailyactive;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class ON extends AppCompatActivity {
 
@@ -13,17 +11,17 @@ public class ON extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_on);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+        Button back = (Button) findViewById(R.id.btnBackon);
+
+        back.setOnClickListener(new View.OnClickListener() {
+                                    public void onClick(View arg0) {
+                                        finish();
+                                    }
+                                }
+
+        );
     }
 
 }
