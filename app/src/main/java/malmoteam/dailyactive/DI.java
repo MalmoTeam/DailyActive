ed3650fc14380f698e0dd3841fe2488678ef85c1
wasim <wasimijaz@hotmail.com>
2016-03-18 11:12:15 +0100

    added back button in DI activity so by clicking that, it,will go back to main screen

--- app/src/main/java/malmoteam/dailyactive/DI.java
+++ app/src/main/java/malmoteam/dailyactive/DI.java
@@ -1,11 +1,9 @@
 package malmoteam.dailyactive;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class DI extends AppCompatActivity {
 
@@ -13,17 +11,15 @@ public class DI extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_di);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        Button back = (Button) findViewById(R.id.btnBackdi);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        back.setOnClickListener(new View.OnClickListener() {
+                                    public void onClick(View arg0) {
+                                        finish();
+                                    }
+                                }
+
+        );
     }
 
 }
